9857abf63d99947a72f2ae3f2884f54a879353ce
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-16 10:23:25 +0200

    Getting User Input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,31 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+import { useState, useRef } from "react";
+
+const SimpleInput = props => {
+	const [enteredName, setEnteredName] = useState("");
+
+	const nameInputChangeHandler = event => {
+		setEnteredName(event.target.value);
+	};
+
+  const formSubmissionHandler = event =>{
+    event.preventDefault();
+    console.log(enteredName)
+
+
+    setEnteredName('')
+  };
+
+	return (
+		<form onSubmit={formSubmissionHandler}>
+			<div className='form-control'>
+				<label htmlFor='name'>Your Name</label>
+				<input type='text' id='name' onChange={nameInputChangeHandler} />
+			</div>
+			<div className='form-actions'>
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
