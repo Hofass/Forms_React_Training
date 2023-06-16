6fccc79bba765c0231faed63dd45700c3895b8b1
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-16 23:47:13 +0200

    React to lost focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -13,6 +13,15 @@ const SimpleInput = props => {
 
 	const nameInputChangeHandler = event => {
 		setEnteredName(event.target.value);
+		
+	};
+
+	const nameInputBlurHandler = event =>{
+		setEnteredNameTouched(true);
+		if (enteredName.trim() === "") {
+			setEnteredNameIsValid(false);
+			return;
+		}
 	};
 
 	const formSubmissionHandler = event => {
@@ -39,6 +48,7 @@ const SimpleInput = props => {
 					type='text'
 					id='name'
 					onChange={nameInputChangeHandler}
+					onBlur={nameInputBlurHandler}
 					value={enteredName}
 				/>
 				{nameInputIsInvalid && <p>Name must not be empty</p>}
